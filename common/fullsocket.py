ce9910e09c463f045777e0385132c0215030597f
Daniele Parmeggiani <dani.parmeggiani@gmail.com>
2017-02-05 18:29:20 +0100

    Module common.fullsocket is now compatible with Python 3.

--- common/fullsocket.py
+++ common/fullsocket.py
@@ -1,8 +1,14 @@
-__author__ = 'paolo'
+__authors__ = ('paolo', 'Daniele Parmeggiani <dani.parmeggiani@gmail.com>')
 # 12/2015
+# Daniele Parmeggiani (compatibility with Python 3): 05/02/2017
 
 import socket
 import struct
+import sys
+
+assert sys.version_info >= (2,5)  # needed for bytes objects
+PY3 = sys.version_info >= (3,0)
+
 
 class FullSocket:
     def __init__(self, sock=None):
@@ -21,28 +27,37 @@ class FullSocket:
         bytes_recd = 0
         while bytes_recd < self.MSGLEN:
             chunk = self.sock.recv(min(self.MSGLEN - bytes_recd, 2048))
-            if chunk == '':
+            if chunk == b'':
                 raise RuntimeError("socket connection broken")
-            chunks.append(chunk)
+            if PY3:
+                chunks.append(chunk.decode())
+            else:
+                chunks.append(chunk)
             bytes_recd = bytes_recd + len(chunk)
         nstr  = ''.join(chunks)
-        nsize = struct.unpack('I',nstr)
+        if PY3:
+            nsize = struct.unpack('I', nstr.encode())
+        else:
+            nsize = struct.unpack('I', nstr)
         size  = socket.ntohl(nsize[0])
         chunks = []
         bytes_recd = 0
         while bytes_recd < size:
             chunk = self.sock.recv(min(size - bytes_recd, 2048))
-            if chunk == '':
+            if chunk == b'':
                 raise RuntimeError("socket connection broken")
-            chunks.append(chunk)
+            if PY3:
+                chunks.append(chunk.decode())
+            else:
+                chunks.append(chunk)
             bytes_recd = bytes_recd + len(chunk)
         return ''.join(chunks)
 
     def send(self, msg):
-        size=len(msg)               # send msg length as unsigned integer
-        nsize=socket.htonl(size)    #
-        nstr=struct.pack('I',nsize)
-        sizelen=len(nstr)
+        size = len(msg)               # send msg length as unsigned integer
+        nsize = socket.htonl(size)    #
+        nstr = struct.pack('I',nsize)
+        sizelen = len(nstr)
         totalsent = 0
         while totalsent < sizelen:
             sent = self.sock.send(nstr[totalsent:])
@@ -56,4 +71,3 @@ class FullSocket:
             if sent == 0:
                 raise RuntimeError("socket connection broken")
             totalsent = totalsent + sent
-
